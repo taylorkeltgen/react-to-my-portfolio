43c321223e0018f59c4d3532b6dac38fede079e7
taylorkeltgen <taylorkeltgen@gmail.com>
2022-03-06 00:32:14 -0600

    updated spacing on AboutMe picture.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import portrait from '../../assets/images/portrait.webp';
 
-
 function About() {
   return (
     <section className="container">
-      <h1 id="about" className="page-title ">
-        About Me
-      </h1>
-      <img className='' src={portrait} alt="self portrait" width={250} height={300} />
+      <div className='d-flex justify-content-between'>
+        <h1 id="about" className="page-title ">
+          About Me
+        </h1>
+        <img className="" src={portrait} alt="self portrait" width={250} height={300} />
+      </div>
       <div className="my-2">
-        <p>
-          Hi my name is Taylor Keltgen
-        </p>
+        <p>Hi my name is Taylor Keltgen. I am an aspiring Developer currently working in the IT Support field.</p>
       </div>
     </section>
   );
